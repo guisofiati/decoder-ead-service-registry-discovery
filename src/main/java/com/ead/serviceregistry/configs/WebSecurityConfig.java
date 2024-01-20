b532b45a01f87366f8379c6c2e64eea1d780c0ca
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2024-01-20 13:26:09 -0300

    spring 3.0 and java 17

--- src/main/java/com/ead/serviceregistry/configs/WebSecurityConfig.java
+++ src/main/java/com/ead/serviceregistry/configs/WebSecurityConfig.java
@@ -3,7 +3,9 @@ package com.ead.serviceregistry.configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,13 +25,10 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic()
-                .and()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable()
-                .formLogin();
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
